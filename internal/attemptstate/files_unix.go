7f2b565ec3d9ed65e9c976bcf23225fdb8454fa7
Ian Rodrigues <ian@rdgs.net>
2026-09-13 13:35:57 +0100

    fix(attemptstate): harden state locking and validation

--- internal/attemptstate/files_unix.go
+++ internal/attemptstate/files_unix.go
@@ -64,14 +64,15 @@ func validateLeaf(path string) error {
 	if !info.Mode().IsRegular() {
 		return errors.New("state leaf must be a regular file")
 	}
-	return nil
+	return validatePrivateOwnedFile(info)
 }
 
 func openAndLock(path string) (*os.File, error) {
-	if err := validateLeaf(path); err != nil {
-		return nil, err
+	fd, err := syscall.Open(path, syscall.O_CREAT|syscall.O_EXCL|syscall.O_RDWR|syscall.O_NOFOLLOW|syscall.O_CLOEXEC, 0600)
+	created := err == nil
+	if errors.Is(err, syscall.EEXIST) {
+		fd, err = syscall.Open(path, syscall.O_RDWR|syscall.O_NOFOLLOW|syscall.O_CLOEXEC, 0)
 	}
-	fd, err := syscall.Open(path, syscall.O_CREAT|syscall.O_RDWR|syscall.O_NOFOLLOW, 0600)
 	if err != nil {
 		return nil, err
 	}
@@ -82,7 +83,18 @@ func openAndLock(path string) (*os.File, error) {
 		_ = file.Close()
 		return nil, errors.New("lock file changed during open")
 	}
-	if err := file.Chmod(0600); err != nil {
+	if created {
+		if err := file.Chmod(0600); err != nil {
+			_ = file.Close()
+			return nil, err
+		}
+		opened, statErr = file.Stat()
+		if statErr != nil {
+			_ = file.Close()
+			return nil, statErr
+		}
+	}
+	if err := validatePrivateOwnedFile(opened); err != nil {
 		_ = file.Close()
 		return nil, err
 	}
@@ -91,6 +103,48 @@ func openAndLock(path string) (*os.File, error) {
 		return nil, fmt.Errorf("supervisor is already using this state path: %w", err)
 	}
 	return file, nil
+}
+
+func lockDirectory(directory *os.File) error {
+	if err := syscall.Flock(int(directory.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		return fmt.Errorf("supervisor is already using this state directory: %w", err)
+	}
+	return nil
+}
+
+func unlockDirectory(directory *os.File) error {
+	return syscall.Flock(int(directory.Fd()), syscall.LOCK_UN)
+}
+
+func validatePrivateDirectory(info os.FileInfo) error {
+	if !info.IsDir() {
+		return errors.New("attempt state path is not a directory")
+	}
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok || stat.Uid != uint32(os.Geteuid()) {
+		return errors.New("attempt state directory is not owned by the current user")
+	}
+	if info.Mode().Perm()&0077 != 0 || info.Mode().Perm()&0700 != 0700 {
+		return errors.New("attempt state directory must be private and writable by its owner")
+	}
+	return nil
+}
+
+func validatePrivateOwnedFile(info os.FileInfo) error {
+	if !info.Mode().IsRegular() {
+		return errors.New("state leaf must be a regular file")
+	}
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok || stat.Uid != uint32(os.Geteuid()) {
+		return errors.New("state leaf is not owned by the current user")
+	}
+	if stat.Nlink != 1 {
+		return errors.New("state leaf must not have multiple hard links")
+	}
+	if info.Mode().Perm()&0077 != 0 {
+		return errors.New("state leaf must be private")
+	}
+	return nil
 }
 
 func unlockAndClose(file *os.File) error {
@@ -103,7 +157,7 @@ func unlockAndClose(file *os.File) error {
 }
 
 func openReadNoFollow(path string) (*os.File, error) {
-	fd, err := syscall.Open(path, syscall.O_RDONLY|syscall.O_NOFOLLOW, 0)
+	fd, err := syscall.Open(path, syscall.O_RDONLY|syscall.O_NOFOLLOW|syscall.O_CLOEXEC, 0)
 	if err != nil {
 		return nil, err
 	}
@@ -118,7 +172,7 @@ func openReadNoFollow(path string) (*os.File, error) {
 }
 
 func createPrivateFile(path string) (*os.File, error) {
-	fd, err := syscall.Open(path, syscall.O_WRONLY|syscall.O_CREAT|syscall.O_EXCL|syscall.O_NOFOLLOW, 0600)
+	fd, err := syscall.Open(path, syscall.O_WRONLY|syscall.O_CREAT|syscall.O_EXCL|syscall.O_NOFOLLOW|syscall.O_CLOEXEC, 0600)
 	if err != nil {
 		return nil, err
 	}
