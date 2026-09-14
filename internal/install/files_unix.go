e6443d427608e98a11e48edf687d021516d22724
Ian Rodrigues <ian@rdgs.net>
2026-09-14 10:19:51 +0100

    feat(setup): verify and activate Go releases

---
+++ internal/install/files_unix.go
@@ -0,0 +1,24 @@
+//go:build unix
+
+package install
+
+import (
+	"os"
+	"syscall"
+)
+
+func fileUID(info os.FileInfo) int {
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return -1
+	}
+	return int(stat.Uid)
+}
+
+func fileNlink(info os.FileInfo) uint64 {
+	stat, ok := info.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0
+	}
+	return uint64(stat.Nlink)
+}
